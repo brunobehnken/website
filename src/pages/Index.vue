022ff748eb775a50f089046ccdc89932030ed8ba
Bruno Behnken <brunobehnken@gmail.com>
2021-07-05 17:58:54 -0300

    Removing useless components

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -136,18 +136,6 @@
 </template>
 <script>
 import { Parallax } from '@/components';
-import BasicElements from './components/BasicElementsSection';
-import Navigation from './components/Navigation';
-import TabsSection from './components/Tabs';
-import ProgressPagination from './components/ProgressPagination';
-import Notifications from './components/Notifications';
-import Typography from './components/Typography';
-import JavascriptComponents from './components/JavascriptComponents';
-import CarouselSection from './components/CarouselSection';
-import NucleoIconsSection from './components/NucleoIconsSection';
-import SignupForm from './components/SignupForm';
-import ExamplesSection from './components/ExamplesSection';
-import DownloadSection from './components/DownloadSection';
 
 export default {
   name: 'index',
